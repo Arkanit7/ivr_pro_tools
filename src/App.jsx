7eac00e0e0e4b7983981522e57cb6ef7eae256f0
BorisBedrenko <wadeforce@gmail.com>
2026-04-04 00:57:31 +0300

    feat: init

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Excel from '@/components/Excel'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-xl">IVR PRO Tools</h1>
-      <input
-        className="cursor-pointer rounded-lg bg-zinc-950 text-white"
-        type="file"
-        name="excel"
-      />
+      <Excel />
     </>
   )
 }
---
+++ src/lib/ttsNormalize.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param {string} text
+ * @returns {string}
+ */
+export default function ttsNormalize(text) {
+  return text.toUpperCase()
+}
