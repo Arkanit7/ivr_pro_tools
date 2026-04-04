11a5a9a0c6cf6842605e172a938d02f02ab714ec
BorisBedrenko <wadeforce@gmail.com>
2026-04-04 19:19:45 +0300

    build: add single file build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import {defineConfig} from 'vite'
 import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import {viteSingleFile} from 'vite-plugin-singlefile'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -17,7 +18,22 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
-  plugins: [react(), babel({presets: [reactCompilerPreset()]}), tailwindcss()],
+  plugins: [
+    react(),
+    babel({presets: [reactCompilerPreset()]}),
+    tailwindcss(),
+    viteSingleFile(),
+  ],
+  base: './',
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    rollupOptions: {
+      inlineDynamicImports: true,
+    },
+  },
   define: {
     global: 'globalThis',
   },
