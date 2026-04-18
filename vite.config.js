b44459c645563428051c65b84b69d72cee6d7b6d
BorisBedrenko <wadeforce@gmail.com>
2026-04-18 18:00:45 +0300

    build: migrate to tsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import {dirname, resolve} from 'node:path'
-import {fileURLToPath} from 'node:url'
 import {defineConfig} from 'vite'
 import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import {viteSingleFile} from 'vite-plugin-singlefile'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 /** @see https://vite.dev/config/ */
 export default defineConfig({
@@ -14,9 +10,7 @@ export default defineConfig({
     open: true,
   },
   resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
+    tsconfigPaths: true,
   },
   plugins: [
     react(),
